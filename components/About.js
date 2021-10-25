c1fee29d887ec9b31fafea312b3fdc68d4f85ff6
tom318267 <ogar318267@gmail.com>
2021-10-25 13:54:04 -0400

    adust grammar in about section

--- components/About.js
+++ components/About.js
@@ -7,24 +7,24 @@ const About = () => {
         <h1 className="text-3xl mb-8">About Me</h1>
         <p className="mb-8 text-lg">
           My name is Tom Rago. I am a self-taught web developer. I started
-          learning about three years ago. I was stuck in a dead-end job and
-          wasn’t very happy with where my career was going. I searched and
-          searched on the internet to find a career I could learn on own. There
-          were decent jobs that I found, but nothing I was interested in. Then
-          web development caught my eye. I was always interested in technology
-          and the web, so I figured I would give it a shot. After reading up on
-          others that have switched careers to web development, I found that it
-          was possible. It was inspiring. If I put in the hard work and tried my
+          learning about three years ago. I was stuck in a dead-end job and was
+          not very happy with where my career was going. I searched and searched
+          on the internet to find a career I could learn on my own. There were
+          decent jobs that I found, but nothing I was interested in. Then web
+          development caught my eye. I was always interested in technology and
+          the web, so I figured I'd give it a shot. After reading up on others
+          that have switched careers to web development, I found that it was
+          possible. It was inspiring. If I put in the hard work and tried my
           best, I could finally have a career I was proud of and make a great
-          living for myself.I have taken courses online such as Udemy,
+          living for myself. I've taken courses online such as Udemy,
           Codeacademy, and Treehouse. I found that Udemy was the best learning
-          tool for me and was not expensive at all. They have great teachers,
-          and if you have any questions about the course or run into any
-          problems, they will respond rather swiftly. I have learned HTML, CSS,
-          Javascript, React, Redux, Nodejs, and many more libraries/frameworks.
-          I enjoy creating websites and would love to make it my new career. All
-          I need is someone to give me a chance and I know I will become a great
-          asset to any company willing to give me the opportunity.
+          tool for me and wasn't expensive at all. They have great teachers, and
+          if you have any questions about the course or run into any problems,
+          they'll respond rather swiftly. I've learned HTML, CSS, Javascript,
+          React, Redux, Nodejs, and many more libraries/frameworks. I enjoy
+          creating websites and would love to make it my new career. All I need
+          is someone to give me a chance and I know I'll become a great asset to
+          any company willing to give me the opportunity.
         </p>
       </div>
 
